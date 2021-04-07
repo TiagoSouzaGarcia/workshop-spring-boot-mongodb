9e954e8e2d39f270a57d0f2fa5598a9ec41105e7
TiagoSouzaGarcia <tiago.s.garcia@gmail.com>
2021-04-07 15:29:43 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/br/com/tiagosouzagarcia/workshopmongo/config/Instatiation.java
@@ -0,0 +1,31 @@
+package br.com.tiagosouzagarcia.workshopmongo.config;
+
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.tiagosouzagarcia.workshopmongo.domain.User;
+import br.com.tiagosouzagarcia.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instatiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+}
