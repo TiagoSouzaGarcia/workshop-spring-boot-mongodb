9e2fd5f8c844c1acb523a6224f304b716adfbe2e
TiagoSouzaGarcia <tiago.s.garcia@gmail.com>
2021-08-25 11:42:34 -0300

    Consulta com vários critérios

--- src/main/java/br/com/tiagosouzagarcia/workshopmongo/resources/PostResource.java
+++ src/main/java/br/com/tiagosouzagarcia/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package br.com.tiagosouzagarcia.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,23 +16,34 @@ import br.com.tiagosouzagarcia.workshopmongo.resources.util.URL;
 import br.com.tiagosouzagarcia.workshopmongo.services.PostService;
 
 @RestController
-@RequestMapping(value="/posts")
+@RequestMapping(value ="/posts")
 public class PostResource {
 
 	@Autowired
 	private PostService service;
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
-	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
-	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+
+	@RequestMapping(value ="/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value ="text", defaultValue ="") String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
 		return ResponseEntity.ok().body(list);
 	}
+
+	@RequestMapping(value ="/fullsearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value = "text", defaultValue ="") String text,
+			@RequestParam(value = "minDate", defaultValue ="") String minDate,
+			@RequestParam(value = "maxDate", defaultValue ="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}
 }
-	
