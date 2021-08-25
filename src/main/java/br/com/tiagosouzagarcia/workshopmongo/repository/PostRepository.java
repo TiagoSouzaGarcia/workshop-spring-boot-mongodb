efbb15304aecd36275d616a66e1f0fbfcaeae1d5
TiagoSouzaGarcia <tiago.s.garcia@gmail.com>
2021-08-25 10:42:00 -0300

    Consulta simples com @Query

--- src/main/java/br/com/tiagosouzagarcia/workshopmongo/repository/PostRepository.java
+++ src/main/java/br/com/tiagosouzagarcia/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package br.com.tiagosouzagarcia.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.tiagosouzagarcia.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	@Query("{ 'title': {$regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
